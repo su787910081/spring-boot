4580ccda5bc91bf61938d76c5fcbb60387fb5e01
suyh <su_787910081@sina.com>
2021-02-16 13:00:37 +0800

    编译成功

---
+++ spring-boot-tests/test-demo-suyh-01/src/main/java/com/suyh/test01/TestDemoMvc.java
@@ -0,0 +1,12 @@
+package com.suyh.test01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemoMvc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemoMvc.class, args);
+	}
+}
